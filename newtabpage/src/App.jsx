d589283b4930ce4ff9a96ef7995b78d8a538fd24
abcde456 <yellieth2575@hwdsb.on.ca>
2026-07-18 17:38:06 -0400

    Fixed focus

--- newtabpage/src/App.jsx
+++ newtabpage/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
 
 function App() {
+    const searchRef = useRef(null);
+
     return (
         <div className="maincontainer">
-            <div className="searchbar">
+            <div
+                className="searchbar"
+                onClick={() => searchRef.current.focus()}
+            >
                 <FaSearch />
-                <input placeholder="Search..." />
+                <input ref={searchRef} placeholder="Search..." />
             </div>
             <div className="quicklink"></div>
         </div>
